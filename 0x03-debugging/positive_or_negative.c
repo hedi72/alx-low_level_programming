41ce8edd80b32b48f3a7aa875d39e4b69b9604a1
hedi72 <heditaieb72@gmail.com>
2023-03-23 06:49:00 -0700

    hhh

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,29 +1,27 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	int n;
+#include <stdio.h>
 
+/**
+ *positive_or_negative - prints if a variable is positive or negative
+ *@n: integer
+ *Return: Always 0 (Success)
+ */
+
+void positive_or_negative(int n)
+{
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
 	if (n > 0)
 	{
-	printf("%d is positive\n", n);
+		printf("%d is positive\n", n);
 	}
-	else if (n < 0)
+	else if (n == 0)
 	{
-	printf("%d is negative\n", n);
+		printf("%d is zero\n", n);
 	}
 	else
 	{
-	printf("%d is zero\n", n);
+		printf("%d is negative\n", n);
 	}
-	return (0);
 }
