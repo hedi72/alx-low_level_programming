9c170c7f6fbddc163b7b45c94289bc277b085226
hedi72 <heditaieb72@gmail.com>
2023-03-23 05:58:14 -0700

    first

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+# inclure  " main.h "
+
+/* *
+* main - teste la fonction qui imprime si l'entier est positif ou négatif
+* Retour : 0
+*/
+
+int  principal ( vide )
+{
+	int j ;
+
+	j = 0 ;
+	positif_ou_négatif (i);
+
+	retour ( 0 );
+}
---
+++ 0x03-debugging/positif_ou_négatif.c
@@ -0,0 +1,31 @@
+# inclure  < stdlib.h >
+# inclure  < stdio.h >
+# inclure  < heure.h >
+/* plus d'en-têtes y vont */
+
+/* La doc de style betty pour la fonction main y va */
+/* *
+* main - Détermine si un nombre est positif, négatif ou nul
+*
+* Retour : 0
+*/
+void  positive_or_negative ( int i)
+{
+
+	
+
+
+	si (i > 0 )
+	{
+
+	printf ( " %d est positif \n " , j);
+
+	}
+	sinon  si (i < 0 )
+	{
+		printf ( " %d est négatif \n " , j);
+	}
+	autre
+	printf ( " %d vaut zéro \n " , j);
+
+}
