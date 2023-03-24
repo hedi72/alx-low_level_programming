f7a75cea945ee2934a1f8e1cc5764599b9e98d49
hedi72 <heditaieb72@gmail.com>
2023-03-24 16:57:23 -0700

    print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,15 +1,21 @@
-#include " main.h"
+#include "main.h"
 
 /**
- * main - check the code for Holberton School students.
+ * print_line - draws a straight line in the terminal
  *
- * Return: Always 0.
+ * @n: the number of _
+ * Result: void
  */
-int main(void)
+void print_line(int n)
 {
-print_line(0);
-print_line(2);
-print_line(10);
-print_line(-4);
-return (0);
+	int i;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 0; i < n; i++)
+			_putchar('_');
+		_putchar('\n');
+	}
 }
