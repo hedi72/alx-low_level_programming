71bd15449a9eaf9f9818f7547ecfde68b11f0882
hedi72 <heditaieb72@gmail.com>
2023-03-27 18:08:53 -0700

    advanced 2

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -11,15 +11,15 @@
  */
 int main(void)
 {
-	char pass[84];
+	char password[84];
 	int index = 0, sum = 0, diff_half1, diff_half2;
 
 	srand(time(0));
 
 	while (sum < 2772)
 	{
-		pass[index] = 33 + rand() % 94;
-		sum += pass[index++];
+		password[index] = 33 + rand() % 94;
+		sum += password[index++];
 	}
 
 	password[index] = '\0';
@@ -31,25 +31,25 @@ int main(void)
 		if ((sum - 2772) % 2 != 0)
 			diff_half1++;
 
-		for (index = 0; pass[index]; index++)
+		for (index = 0; password[index]; index++)
 		{
-			if (pass[index] >= (33 + diff_half1))
+			if (password[index] >= (33 + diff_half1))
 			{
-				pass[index] -= diff_half1;
+				password[index] -= diff_half1;
 				break;
 			}
 		}
-		for (index = 0; pass[index]; index++)
+		for (index = 0; password[index]; index++)
 		{
-			if (pass[index] >= (33 + diff_half2))
+			if (password[index] >= (33 + diff_half2))
 			{
-				pass[index] -= diff_half2;
+				password[index] -= diff_half2;
 				break;
 			}
 		}
 	}
 
-	printf("%s", pass);
+	printf("%s", password);
 
 	return (0);
 }
