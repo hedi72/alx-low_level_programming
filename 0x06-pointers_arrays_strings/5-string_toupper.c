fc220519caea37bf61620b8940b7d15466e336f8
hedi72 <heditaieb72@gmail.com>
2023-03-30 18:19:14 -0700

    5-string_toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*string_toupper - changes lowercase to uppercase
+*@st: string
+*Return: pointer to char
+*/
+char *string_toupper(char *st)
+{
+int i = 0;
+while(st[i] != '\0')
+{
+if (st[i] >= 'a' && st[i] <= 'z')
+{
+st[i] = st[i] - ('a' - 'A');
+}
+i++;
+}
+    return (st);
+}
