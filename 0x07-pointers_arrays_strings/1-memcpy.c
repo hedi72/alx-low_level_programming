94791095adbfced4fd8c73e5c6696a7ddb3d7477
hedi72 <heditaieb72@gmail.com>
2023-04-03 07:27:59 -0700

    fixed : function memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*_memcpy - copies n bytes from memory area src to memory area dest
+*@dest: pointer to output string
+*@src: input string
+*@n: number of bytes that will be copied from src
+*Return: pointer to a string
+*/
+char *_memcpy(char *dest, char *src, unsigned int n) {
+unsigned int i = 0;
+while (i++ < n) {
+dest[i] = src[i];
+}
+return dest;
+}
