12200a3d290744fa15e3ef0644c03f273dd94bbd
hedi72 <heditaieb72@gmail.com>
2023-04-12 07:24:02 -0700

    optimisation du code

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,25 +13,21 @@ char *create_array(unsigned int size, char c)
 {
 unsigned int i;
 char *p;
-
 if (size == 0)
 {
 return (NULL);
 }
-else
-{
+
 p = malloc(sizeof(char) * size);
 if (p == NULL)
 {
 return (NULL);
 }
-else
-{
+
 for (i = 0; i < size; i++)
 {
 p[i] = c;
 }
+
 return (p);
-}
-}
 }
