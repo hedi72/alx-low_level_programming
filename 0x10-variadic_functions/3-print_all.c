6a7fa1c60df8420aaf49de3329d51d336e604bfa
hedi72 <heditaieb72@gmail.com>
2023-04-20 16:37:12 -0700

    fixed : function that

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,40 +1,21 @@
-
 #include <stdarg.h>
 #include <stdio.h>
 #include "variadic_functions.h"
 
-/**
- * print_char - use va arg to print the char
- * @c: char
-*/
 void print_char(va_list c)
 {
 putchar(va_arg(c, int));
 }
-
-/**
-*print_integer - use va arg to print the integer
-*@i: int
-*/
 
 void print_integer(va_list i)
 {
 printf("%d", va_arg(i, int));
 }
 
-/**
-*print_float - use va arg to print float
-*@f: float
-*/
-
 void print_float(va_list f)
 {
 printf("%f", va_arg(f, double));
 }
-/**
-*print_string - use va arg to print the string or nil
-*@st: string
-*/
 
 void print_string(va_list st)
 {
@@ -46,10 +27,6 @@ string = "(nil)";
 printf("%s", string);
 }
 
-/**
-*print_all - prints anything
-*@format: a pointer to string that contains types of parameters
-*/
 void print_all(const char * const format, ...)
 {
 my_str mytypes[] = {
@@ -59,6 +36,7 @@ my_str mytypes[] = {
 {"s", print_string},
 {NULL, NULL}
 };
+
 va_list ls;
 unsigned int i = 0;
 char separator = '\0';
@@ -67,15 +45,16 @@ va_start(ls, format);
 
 while (format && format[i])
 {
+j = 0;
+while (mytypes[j].letter != NULL)
+{
+if (format[i] == *(mytypes[j].letter))
+{
 if (separator != '\0')
 {
 putchar(separator);
 }
 
-while (mytypes[j].letter != NULL)
-{
-if (format[i] == *(mytypes[j].letter))
-{
 mytypes[j].f(ls);
 separator = ',';
 break;
