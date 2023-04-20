7d29ac87c6f534813598895607ec1cbc43cb352d
hedi72 <heditaieb72@gmail.com>
2023-04-20 10:52:16 -0700

    fixed: function that returns the sum of all its params

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,24 @@
+#ifndef _VARIADIC_FUNCTION_H_
+#define _VARIADIC_FUNCTION_H_
+
+#include <stdarg.h>
+
+/**
+ * struct types - pointer to function with corresponding letter
+ * @letter: type name
+ * @f: print function
+ */
+typedef struct types
+{
+char *letter;
+void (*f)(va_list);
+} my_str;
+
+
+int _putchar(char);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif 
