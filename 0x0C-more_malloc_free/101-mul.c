57e5cb3ade0123bb3f4851c077c117eff55ab1cd
hedi72 <heditaieb72@gmail.com>
2023-04-28 09:11:40 -0700

    last task

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,18 +2,19 @@
 #include <stdlib.h>
 
 int multiply(int num1, int num2){
-return (num1 * num2)
+return (num1 * num2);
 }
 
 int main(int argc, char *argv[])
 {
+int num1, num2;
 if (argc != 3)
 {
 printf("Error \n");
 return (98);
 }
-int num1 = atoi(argv[1]);
-int num2 = atoi(argv[2]);
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
 
 if (num1 <= 0 || num2 <= 0) {
 printf("Error\n");
