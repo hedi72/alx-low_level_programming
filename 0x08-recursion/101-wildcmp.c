9a7f345882000fabbb8d73842b5d789c27da92b9
hedi72 <heditaieb72@gmail.com>
2023-04-28 07:02:27 -0700

    last commit

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -10,18 +10,18 @@ int wildcmp(char *s1, char *s2)
 {
 if(*s2 == '*')
 {
-if(*s1 == NULL && *(s2+1) == NULL)
+if(*s1 == '\0' && *(s2+1) == '\0')
 return (1);
-else if (*s1 == NULL || b*(s2 + 1) == NULL)
+else if (*s1 == '\0' || *(s2 + 1) == '\0')
 return (0);
 else if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
 return (1);
 else
 return (0);
 }
-else if (*s1 == NULL)
+else if (*s1 == '\0')
 {
-if (*s2 == NULL)
+if (*s2 == '\0')
 return (1);
 else
 return (0);
