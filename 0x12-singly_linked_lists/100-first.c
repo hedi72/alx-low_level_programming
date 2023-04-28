74b33247b0fc400734ac0b4d93271daa3f35c02d
hedi72 <heditaieb72@gmail.com>
2023-04-28 13:38:16 -0700

    100-first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * Before_main - function executed
+ */
+
+void Before_main(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
