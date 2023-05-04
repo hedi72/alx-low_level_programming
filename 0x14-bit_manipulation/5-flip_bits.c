6c1a1e24e82595e635d62ecdb456010ff99d863b
hedi72 <heditaieb72@gmail.com>
2023-05-04 07:24:10 -0700

    task 5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -10,10 +10,10 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 unsigned long int diff = n ^ m;
 unsigned int count = 0;
-while (diff > 0)
+while (diff)
 {
 count += diff & 1;
-diff >>= 1;
+diff = >> 1;
 }
 return (count);
 }
