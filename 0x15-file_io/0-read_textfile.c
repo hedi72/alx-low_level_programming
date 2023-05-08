2ec98d5a172319c854fdf895b8875b5fa888f41c
hedi72 <heditaieb72@gmail.com>
2023-05-08 06:32:15 -0700

    function that reads a text file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+ssize_t file_descriptor, bytes_read, bytes_written;
+char *buffer;
+if (filename == NULL)
+return (0);
+file_descriptor = open(filename, O_RDONLY);
+if (file_descriptor == -1)
+return (0);
+buffer = malloc(sizeof(char) * (letters + 1));
+if (buffer == NULL)
+return (0);
+
+bytes_read = read(file_descriptor, buffer, letters);
+if (bytes_read == -1)
+{
+   
+free(buffer);
+close(file_descriptor);
+return 0;
+}
+
+buffer[bytes_read] = '\0';
+bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+if (bytes_written == -1 || bytes_written != bytes_read)
+{
+free(buffer);
+close(file_descriptor);
+return (0);
+}
+
+free(buffer);
+close(file_descriptor);
+return (bytes_written);
+}
